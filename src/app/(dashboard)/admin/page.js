e502e92a91907b1e49e4c8a8207ad68cdb315bf8
rh-ralib04 <rakibulhossainbhuiya04@gmail.com>
2026-03-03 11:50:54 +0600

    [ADDED] Admin stats API endpoint with aggregation

--- src/app/(dashboard)/admin/page.js
+++
@@ -1,9 +0,0 @@
-export default function AdminDashboard() {
-  return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-6">Admin Dashboard</h1>
-      {/* Admin dashboard content */}
-    </div>
-  );
-}
-
